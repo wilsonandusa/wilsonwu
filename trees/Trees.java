d7a96279e0ffc77d7cf31d8c1a41fac2cd94979b
WilsonWu <wilsonandiphone@icloud.com>
2015-10-12 01:19:26 -0500

    Definition of Trees

---
+++ trees/Trees.java
@@ -0,0 +1,22 @@
+//data structure for trees and graphs
+
+//class definition for Nodes
+class Node{
+  Public String name;
+  Public Node[] children;
+}
+class Tree{
+  public Node root;
+}
+
+//A binary tree is a tree in which each node has up to two children. Not all trees are binary trees. 
+//A binary search tree is a binary tree in which every node fits a specific ordering property:
+//all left descendents <= n < all right descendents.
+
+//Complete Binary Trees: a complete binary tree is a binary tree in which every level of the tree is fully filled, except perhaps for the last
+// last level. To the extent that the last level is filled, it is filled left to right.
+
+//Full binary Tree.
+
+//Perfect Binary Tree.
+
