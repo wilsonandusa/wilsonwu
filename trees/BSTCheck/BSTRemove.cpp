95a1ccdb3a5e92bd70c70052b164f25798a1e5de
WilsonWu <wilsonandiphone@icloud.com>
2015-10-21 21:04:55 -0500

    Update BSTRemove.cpp

--- trees/BSTCheck/BSTRemove.cpp
+++ trees/BSTCheck/BSTRemove.cpp
@@ -1,10 +1,43 @@
 void BST<K>::remove(treeNode* & cRoot, const K & k){
   if(cRoot!=NULL){
-  if(croot->key == k)
+  if(cRoot->key == k)
   doRemoval(cRoot);
   else if (k<croot->key)
   remove(cRoot->left,k);
   else
   remove(cRoot->right,k);
 }
+}
+
+void BST<K>::doRemoval (treeNode* & cRoot){
+  if((cRoot->left==NULL))&&(cRoot->right==NULL)
+  noChildRemove(cRoot);
+  else if((cRoot->left!=NULL)&&)(cRoot->right!=NULL)
+  twoChildRemove(cRoot);
+  else
+  oneChildRemove(cRoot);
+}
+
+void BST<K>:::noChildRemove(treeNode* & cRoot){
+  treeNode* temp = cRoot;
+  cRoot=NULL;
+  delete temp;
+}
+
+void BST<K>::oneChildRemove(treeNode* & cRoot){
+  treeNode* temp = cRoot;
+  if(cRoot->left==NULL) cRoot = cRoot->right;
+  else cRoot = cRoot->left;
+  delete temp;
+}
+
+void BST<k>::twoChildeRemove (treeNode * & cRoot){
+  treeNode* & iop = rightMostChild(cRoot->left);
+  cRoot->key = iop -> key;
+  doRemoval(iop);
+}
+
+treeNode * & BST<k>::rightmostChild(treeNode* & cRoot){
+  if(cRoot->right == NULL) returm cRoot;
+  else return rightMostChild(cRoot->right);
 }
