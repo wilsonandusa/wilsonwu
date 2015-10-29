1952515943694bfe0705ecab9017ecd854a6cb45
WilsonWu <wilsonandiphone@icloud.com>
2015-10-28 21:17:51 -0500

    Update queuefuncs.cpp

--- Stack-Queue/queuefuncs.cpp
+++ Stack-Queue/queuefuncs.cpp
@@ -1,1 +1,24 @@
 
+//********************************BACK**************************
+//returns a reference to the last element in the queue
+//This is the newest element in the queue;the last element pushed into the stack
+
+// queue::back
+#include <iostream>       // std::cout
+#include <queue>          // std::queue
+
+int main ()
+{
+  std::queue<int> myqueue;
+
+  myqueue.push(12);
+  myqueue.push(75);   // this is now the back
+
+  myqueue.back() -= myqueue.front();
+
+  std::cout << "myqueue.back() is now " << myqueue.back() << '\n';
+
+  return 0;
+}
+//myqueue.back() is now 63
+//***************************emplace*********************
