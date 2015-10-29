6835c0183919d8ffdaaeeef072ec0de8498eb95b
WilsonWu <wilsonandiphone@icloud.com>
2015-10-28 21:10:39 -0500

    Create stackfuncs.cpp

---
+++ Stack-Queue/stackfuncs.cpp
@@ -0,0 +1,17 @@
+//
+stack::emplace
+//adds a new element at the top of the stack, above its current top element 
+stack::empty
+while(!myStack.empty()){
+}
+
+//******************
+stack::pop
+
+stack::push
+
+stack::size
+
+stack::swap
+
+stack::top
