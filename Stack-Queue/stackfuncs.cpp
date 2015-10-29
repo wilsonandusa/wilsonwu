e49e2882639ba1d00d723d5f0a84f2a2d47d1154
WilsonWu <wilsonandiphone@icloud.com>
2015-10-28 21:15:37 -0500

    Update stackfuncs.cpp

--- Stack-Queue/stackfuncs.cpp
+++ Stack-Queue/stackfuncs.cpp
@@ -1,17 +1,37 @@
 //
 stack::emplace
 //adds a new element at the top of the stack, above its current top element 
-stack::empty
+stack::empty()
 while(!myStack.empty()){
 }
 
 //******************
-stack::pop
+stack::pop()
+//removes the element on the top of the stack, effectively reducing its size by one
 
-stack::push
-
+stack::push()
+//insert a new element at the top of the stack, this member function effectively calls the member 
+//function push_back
 stack::size
-
+//***************************SWAP*****************************************
 stack::swap
+void swap(stack&x); 
+// stack::swap
+#include <iostream>       // std::cout
+#include <stack>          // std::stack
 
+int main ()
+{
+  std::stack<int> foo,bar;
+  foo.push (10); foo.push(20); foo.push(30);
+  bar.push (111); bar.push(222);
+
+  foo.swap(bar);
+
+  std::cout << "size of foo: " << foo.size() << '\n';
+  std::cout << "size of bar: " << bar.size() << '\n';
+
+  return 0;
+}
+//****************************TOP***********************
 stack::top
